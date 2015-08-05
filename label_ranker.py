01d3fd32f157ec26354fbb467a400ed3fe069c8b
Han Xiao <han.xiao@cs.helsinki.fi>
2015-08-05 19:24:43 +0300

    finished label ranker implementation and test

--- label_ranker.py
+++ label_ranker.py
@@ -5,6 +5,8 @@ Reference:
 Qiaozhu Mei, Xuehua Shen, Chengxiang Zhai,
 Automatic Labeling of Multinomial Topic Models, 2007
 """
+import numpy as np
+from scipy.stats import entropy as kl_divergence
 
 
 class LabelRanker(object):
@@ -12,23 +14,21 @@ class LabelRanker(object):
     
     """
     def __init__(self,
-                 intra_topic_coverage=True,
-                 inter_topic_discrimination=True,
+                 apply_intra_topic_coverage=True,
+                 apply_inter_topic_discrimination=True,
                  mu=0.7,
-                 alpha=1.0):
-        self._coverage = intra_topic_coverage
-        self._discrimination = inter_topic_discrimination
+                 alpha=0.9):
+        self._coverage = apply_intra_topic_coverage
+        self._discrimination = apply_inter_topic_discrimination
         self._mu = mu
         self._alpha = alpha
-        
-    def rank(self,
-             topic_models,
-             pmi_w2l,
-             index2label,
-             index2word,
-             top_n=5
-             ):
+
+    def label_relevance_score(self,
+                              topic_models,
+                              pmi_w2l):
         """
+        Calculate the relevance scores between each label and each topic
+
         Parameters:
         ---------------
         topic_models: numpy.ndarray(#topics, #words)
@@ -38,21 +38,183 @@ class LabelRanker(object):
            the Point-wise Mutual Information(PMI) table of
            the form, PMI(w, l | C)
         
+        Returns;
+        -------------
+        numpy.ndarray, shape (#topics, #labels)
+            the scores of each label on each topic
+        """
+        assert topic_models.shape[1] == pmi_w2l.shape[0]
+        return np.asarray(np.asmatrix(topic_models) *
+                          np.asmatrix(pmi_w2l))
+        
+    def label_discriminative_score(self,
+                                   relevance_score,
+                                   topic_models,
+                                   pmi_w2l):
+        """
+        Calculate the discriminative scores for each label
+        
+        Returns:
+        --------------
+        numpy.ndarray, shape (#topics, #labels)
+            the (i, j)th element denotes the score
+            for label j and all topics *except* the ith
+        """
+        assert topic_models.shape[1] == pmi_w2l.shape[0]
+        k = topic_models.shape[0]
+        return (relevance_score.sum(axis=0)[None, :].repeat(repeats=3, axis=0)
+                - relevance_score) / (k-1)
+        
+    def label_mmr_score(self,
+                        which_topic,
+                        chosen_labels,
+                        label_scores,
+                        label_models):
+        """
+        Maximal Marginal Relevance score for labels.
+        It's computed only when `apply_intra_topic_coverage` is True
+
+        Parameters:
+        --------------
+        which_topic: int
+            the index of the topic
+        
+        chosen_labels: list<int>
+           indices of labels that are already chosen
+        
+        label_scores: numpy.ndarray<#topic, #label>
+           label scores for each topic
+
+        label_models: numpy.ndarray<#label, #words>
+            the language models for labels
+
+        Returns:
+        --------------
+        numpy.ndarray: 1D of length #label - #chosen_labels
+            the scored label indices
+
+        numpy.ndarray: same length as above
+            the scores
+        """
+        chosen_len = len(chosen_labels)
+        if chosen_len == 0:
+            # no label is chosen
+            # return the raw scores
+            return (np.arange(label_models.shape[0]),
+                    label_scores[which_topic, :])
+        else:
+            kl_m = np.zeros((label_models.shape[0]-chosen_len,
+                             chosen_len))
+            
+            # the unchosen label indices
+            candidate_labels = list(set(range(label_models.shape[0])) -
+                                    set(chosen_labels))
+            candidate_labels = np.sort(np.asarray(candidate_labels))
+            for i, l_p in enumerate(candidate_labels):
+                for j, l in enumerate(chosen_labels):
+                    kl_m[i, j] = kl_divergence(label_models[l_p],
+                                               label_models[l])
+            sim_scores = kl_m.max(axis=1)
+            mml_scores = (self._alpha *
+                          label_scores[which_topic, candidate_labels]
+                          - (1 - self._alpha) * sim_scores)
+            return (candidate_labels, mml_scores)
+
+    def combined_label_score(self, topic_models, pmi_w2l,
+                             use_discrimination, mu=None):
+        """
+        Calculate the combined scores from relevance_score
+        and discrimination_score(if required)
+
+        Parameter:
+        -----------
+        use_discrimination: bool
+            whether use discrimination or not
+        mu: float
+            the `mu` parameter in the algorithm
+
+        Return:
+        -----------
+        numpy.ndarray, shape (#topics, #labels)
+            score for each topic and label pair
+        """
+        rel_scores = self.label_relevance_score(topic_models, pmi_w2l)
+        
+        if use_discrimination:
+            assert mu != None
+            discrim_scores = self.label_discriminative_score(rel_scores,
+                                                             topic_models,
+                                                             pmi_w2l)
+            label_scores = rel_scores - mu * discrim_scores
+        else:
+            label_scores = rel_scores
+
+        return label_scores
+
+    def select_label_sequentially(self, k_labels,
+                                  label_scores, label_models):
+        """
+        Return:
+        ------------
+        list<list<int>>: shape n_topics x k_labels
+        """
+        n_topics = label_scores.shape[0]
+        chosen_labels = []
+
+        # don't use [[]] * n_topics !
+        for _ in xrange(n_topics):
+            chosen_labels.append(list())
+            
+        for i in xrange(n_topics):
+            for j in xrange(k_labels):
+                inds, scores = self.label_mmr_score(i, chosen_labels[i],
+                                                    label_scores,
+                                                    label_models)
+                chosen_labels[i].append(inds[np.argmax(scores)])
+        return chosen_labels
+
+    def top_k_labels(self,
+                     topic_models,
+                     pmi_w2l,
+                     index2label,
+                     label_models=None,
+                     k=5):
+        """
+        Parameters:
+        ----------------
+        
         index2label: dict<int, object>
            mapping from label index in the `pmi_w2l`
            to the label object, which can be string
 
-        index2word: dict<int, object>
-           mapping from label index in `topic_models`
-           to the word, which is often string
+        label_models: numpy.ndarray<#label, #words>
+            the language models for labels
+            if `apply_intra_topic_coverage` is True,
+            then it's must be given
 
-        top_n: in
-           how many labels returned for each topic model
-        
-        Returns;
-        -------------
-        list<list of <label, float>>
-           #top_n labels as well as scores for each topic model
-        
+        Return:
+        ---------------
+        list<list of (label, float)>
+           top k labels as well as scores for each topic model
+
         """
-        
+
+        assert pmi_w2l.shape[1] == len(index2label)
+
+        label_scores = self.combined_label_score(topic_models, pmi_w2l,
+                                                 self._discrimination,
+                                                 self._mu)
+
+        if self._coverage:
+            assert isinstance(label_models, np.ndarray)
+            # TODO: can be parallel
+            chosen_labels = self.select_label_sequentially(k, label_scores,
+                                                           label_models)
+        else:
+            chosen_labels = np.argsort(label_scores, axis=1)[:, :-k-1:-1]
+
+        return [[index2label[j] for j in topic_i_labels]
+                for topic_i_labels in chosen_labels]
+            
+
+
--- test_label_ranker.py
+++ test_label_ranker.py
@@ -1,18 +1,191 @@
 import numpy as np
+from nose.tools import assert_equal
+from numpy.testing import (assert_array_almost_equal,
+                           assert_array_equal,
+                           assert_almost_equal)
 from label_ranker import LabelRanker
 
 
-def test_label_ranker():
-    ranker = LabelRanker()
-    index2word = ('topic', 'modeling', 'machine', 'learning')
-    index2label = ('topic modeling', 'machine learning')
-    
-    topic_models = np.asarray([[0.4, 0.5, 0.05, 0.05],
-                              [0.05, 0.05, 0.4, 0.5]])
-    pmi_w2l = np.asarray([[1.0, 1.0, 0.2, 0.2],
-                          [0.8, 0.8, 0.3, 0.3]
-                          [-0.2, 0.2, 0.8, 0.8],
-                          [-0.2, 0.0, 0.7, 0.5]])
-    ranker.rank(topic_models, pmi_w2l, index2label, index2word, top_n=1)
+ranker = LabelRanker()
+index2word = ('topic', 'modeling', 'machine', 'learning')
+index2label = ('topic modeling', 'machine learning', 'business management')
 
+topic_models = np.asarray([[0.4, 0.5, 0.1, 0.1],
+                           [0.05, 0.05, 0.4, 0.5],
+                           [0.1, 0.1, 0.1, 0.1]])
+pmi_w2l = np.asarray([[1.0, 0.2, 0],
+                      [0.8, 0.3, 0.1],
+                      [-0.2, 0.8, 0],
+                      [0.0, 0.5, 0.1]])
+label_models = np.asarray([[0.4, 0.4, 0.1, 0.1],
+                           [0.1, 0.2, 0.3, 0.4],
+                           [0.25, 0.25, 0.25, 0.25]])
+
+
+def test_relevance_score():
+    label_relevance_score = ranker.label_relevance_score(topic_models,
+                                                         pmi_w2l)
+    assert_array_almost_equal(label_relevance_score,
+                              np.asarray([[0.78, 0.36, 0.06],
+                                          [0.01, 0.595, 0.055],
+                                          [0.16, 0.18, 0.02]]))
+
+
+def test_mmr_scores():
+    label_scores = ranker.label_relevance_score(topic_models,
+                                                pmi_w2l)
+    actual_indices, actual_scores\
+        = ranker.label_mmr_score(0, [0],  # with 1 label
+                                 label_scores,
+                                 label_models)
+    assert_array_equal(actual_indices, np.asarray([1, 2]))
+    alpha = ranker._alpha
+    assert_array_almost_equal(actual_scores,
+                              np.asarray(
+                                  [alpha*0.36 - (1-alpha)*0.606842558,
+                                   alpha*0.06 - (1-alpha)*0.223143551]
+                              ))
+
+
+def test_mmr_scores_no_chosen_labels():
+    label_scores = ranker.label_relevance_score(topic_models,
+                                                pmi_w2l)
+    actual_indices, actual_scores\
+        = ranker.label_mmr_score(0, [],  # with 0 label
+                                 label_scores,
+                                 label_models)
+    assert_array_equal(actual_indices, np.asarray([0, 1, 2]))
+    assert_array_almost_equal(actual_scores,
+                              np.asarray(
+                                  [0.78, 0.36, 0.06]
+                              ))
+
+
+def test_mmr_scores_max_part():
+    # test the `max` part
+    label_scores = ranker.label_relevance_score(topic_models,
+                                                pmi_w2l)
+    actual_indices, actual_scores\
+        = ranker.label_mmr_score(0, [0, 1],  # with 2 labels
+                                 label_scores,
+                                 label_models)
+    assert_array_equal(actual_indices, np.asarray([2]))
+    alpha = ranker._alpha
+    assert_array_almost_equal(actual_scores,
+                              np.asarray(
+                                  [alpha*0.06 - (1-alpha)*0.223143551]
+                              ))
     
+
+def test_discriminative_scores():
+    label_discriminative_score = ranker.label_discriminative_score(
+        ranker.label_relevance_score(topic_models, pmi_w2l),
+        topic_models,
+        pmi_w2l)
+    assert_array_almost_equal(label_discriminative_score,
+                              np.asarray([[0.085, 0.3875, 0.0375],
+                                          [0.47, 0.27, 0.04],
+                                          [0.395, 0.4775, 0.0575]]))
+    
+
+def test_combined_label_score():
+    label_score = ranker.combined_label_score(topic_models,
+                                              pmi_w2l,
+                                              use_discrimination=True,
+                                              mu=0.7)
+
+    rel_score = np.asarray([[0.78, 0.36, 0.06],
+                            [0.01, 0.595, 0.055],
+                            [0.16, 0.18, 0.02]])
+    dis_score = np.asarray([[0.085, 0.3875, 0.0375],
+                            [0.47, 0.27, 0.04],
+                            [0.395, 0.4775, 0.0575]])
+
+    assert_almost_equal(label_score[0, 0], 0.7205)
+    assert_array_almost_equal(label_score, rel_score - 0.7 * dis_score)
+
+    # without use_discrimination
+    label_score = ranker.combined_label_score(topic_models,
+                                              pmi_w2l,
+                                              use_discrimination=False)
+    assert_array_almost_equal(label_score, rel_score)
+
+
+def test_select_label_sequentially():
+    label_score = ranker.combined_label_score(topic_models,
+                                              pmi_w2l,
+                                              use_discrimination=True,
+                                              mu=0.7)
+    actual = ranker.select_label_sequentially(1, label_score, label_models)
+    expected = np.asarray([[0], [1], [2]])
+    assert_array_equal(actual, expected)
+
+
+def test_select_label_sequentially_all_labels():
+    label_score = ranker.combined_label_score(topic_models,
+                                              pmi_w2l,
+                                              use_discrimination=True,
+                                              mu=0.7)
+    actual = ranker.select_label_sequentially(3, label_score, label_models)
+    expected = np.asarray([[0, 1, 2], [1, 2, 0], [2, 0, 1]])
+    assert_array_equal(actual, expected)
+
+
+def test_top_k_labels():
+    labels = ranker.top_k_labels(topic_models, pmi_w2l, index2label,
+                                 label_models, k=1)
+    assert_equal(labels, [['topic modeling'],
+                          ['machine learning'],
+                          ['business management']])
+
+
+def test_top_k_labels_all_labels():
+    labels = ranker.top_k_labels(topic_models, pmi_w2l, index2label,
+                                 label_models, k=3)
+    assert_equal(labels, [['topic modeling', 'machine learning',
+                           'business management'],
+                          ['machine learning', 'business management',
+                           'topic modeling'],
+                          ['business management', 'topic modeling',
+                           'machine learning']])
+
+
+def test_top_k_labels_coverage_OFF_discrim_OFF():
+    ranker = LabelRanker(apply_intra_topic_coverage=False,
+                         apply_inter_topic_discrimination=False)
+    labels = ranker.top_k_labels(topic_models, pmi_w2l, index2label,
+                                 label_models, k=3)
+    assert_equal(labels, [['topic modeling', 'machine learning',
+                           'business management'],
+                          ['machine learning', 'business management',
+                           'topic modeling'],
+                          ['machine learning',  # because discri OFF
+                           'topic modeling',
+                           'business management']])
+
+
+def test_top_k_labels_coverage_OFF_discrim_ON():
+    ranker = LabelRanker(apply_intra_topic_coverage=False,
+                         apply_inter_topic_discrimination=True)
+    labels = ranker.top_k_labels(topic_models, pmi_w2l, index2label,
+                                 label_models, k=3)
+    assert_equal(labels, [['topic modeling', 'machine learning',
+                           'business management'],
+                          ['machine learning', 'business management',
+                           'topic modeling'],
+                          ['business management', 'topic modeling',
+                           'machine learning']])
+
+
+def test_top_k_labels_coverage_ON_discrim_OFF():
+    ranker = LabelRanker(apply_intra_topic_coverage=True,
+                         apply_inter_topic_discrimination=False)
+    labels = ranker.top_k_labels(topic_models, pmi_w2l, index2label,
+                                 label_models, k=3)
+    assert_equal(labels, [['topic modeling', 'machine learning',
+                           'business management'],
+                          ['machine learning', 'business management',
+                           'topic modeling'],
+                          ['machine learning',  # because discri OFF
+                           'topic modeling',
+                           'business management']])
