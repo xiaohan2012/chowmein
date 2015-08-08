4641443a72540651131d2c49d63a554ee4d07622
Han Xiao <han.xiao@cs.helsinki.fi>
2015-08-08 22:33:59 +0300

    label_finder: added POS tag filtering

--- chowmein/label_finder.py
+++ chowmein/label_finder.py
@@ -1,9 +1,11 @@
 import nltk
 from nltk.collocations import BigramCollocationFinder
+from toolz.itertoolz import get
+from toolz.functoolz import partial
 
 
 class BigramLabelFinder(object):
-    def __init__(self, measure='pmi'):
+    def __init__(self, measure='pmi', pos=None):
         """
         measure: str
             the measurement method, 'pmi'or 'chi_sq'
@@ -11,6 +13,8 @@ class BigramLabelFinder(object):
         self.bigram_measures = nltk.collocations.BigramAssocMeasures()
         assert measure in ('pmi', 'chi_sq')
         self._measure_method = measure
+
+        self._pos = pos
     
     def find(self, docs, min_freq, top_n):
         """
@@ -29,14 +33,35 @@ class BigramLabelFinder(object):
         ---------------
         list of tuple of str: the bigrams
         """
-        
+        # if apply pos constraints
+        # check the pos properties
+        if self._pos:
+            assert isinstance(self._pos, list)
+            for pair in self._pos:
+                assert isinstance(pair, tuple) or isinstance(pair, list)
+                assert len(pair) == 2  # because it's bigram
+
+        score_func = getattr(self.bigram_measures,
+                             self._measure_method)
+
         finder = BigramCollocationFinder.from_documents(docs)
         finder.apply_freq_filter(min_freq)
-        bigrams = finder.nbest(getattr(self.bigram_measures,
-                                       self._measure_method),
-                               top_n)
-        labels = []
-        for b in bigrams:
-            labels.append(b)
 
-        return labels
+        if self._pos:
+            valid_pos_tags = set([pair for pair in self._pos])
+            valid_bigrams = []
+            bigrams = map(partial(get, 0),  # get the bigram
+                          finder.score_ngrams(score_func))
+            cnt = 0
+            for bigram in bigrams:
+                if tuple(map(partial(get, 1), bigram)) in valid_pos_tags:
+                    valid_bigrams.append(bigram)
+                    cnt += 1
+                if cnt == top_n:  # enough
+                    break
+            return valid_bigrams
+        else:
+            bigrams = finder.nbest(score_func,
+                                   top_n)
+            return bigrams
+            
--- chowmein/tests/test_label_finder.py
+++ chowmein/tests/test_label_finder.py
@@ -1,10 +1,11 @@
 from chowmein.label_finder import BigramLabelFinder
 from chowmein.data import load_nips
+from chowmein.corpus_processor import CorpusPOSTagger
 from nose.tools import assert_equal
 
 
 def test_label_finder():
-    finder = BigramLabelFinder(measure='pmi')
+    finder = BigramLabelFinder(measure='pmi', pos=None)
     labels = finder.find(load_nips(years=[2009]), 10, top_n=5)
     assert_equal(labels, [(u'monte', u'carlo'),
                           (u'high', u'dimensional'),
@@ -12,3 +13,19 @@ def test_label_finder():
                           (u'experimental', u'results'),
                           (u'nonparametric', u'bayesian')])
     
+
+def test_label_finder_with_pos():
+    tagger = CorpusPOSTagger()
+    finder = BigramLabelFinder(measure='pmi', pos=[('NN', 'NN'),
+                                                   ('JJ', 'NN')])
+
+    docs = load_nips(years=[2009])
+    docs = tagger.transform(docs)
+
+    labels = finder.find(docs, 10, top_n=5)
+    
+    assert_equal(labels, [((u'monte', 'NN'), (u'carlo', 'NN')),
+                          ((u'nonparametric', 'JJ'), (u'bayesian', 'NN')),
+                          ((u'active', 'JJ'), (u'learning', 'NN')),
+                          ((u'machine', 'NN'), (u'learning', 'NN')),
+                          ((u'semi-supervised', 'JJ'), (u'learning', 'NN'))])
